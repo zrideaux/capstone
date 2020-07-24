0daa46c8ad627b3a37d37db3eac57767bb0b7d8b
Kevin Cam <kevinsebcam@gmail.com>
2020-07-24 18:14:56 +0000

    Add an exit button that will make the display of this listing hidden.

--- src/main/webapp/listing.js
+++ src/main/webapp/listing.js
@@ -1,15 +1,35 @@
+// Functions that create html elements
+
 /** 
  * Creates a <div> element containing class and id attribute. 
  *
+ * @param onclickAttribute the js that is executed when this button is clicked
  * @param classAttribute the name of the class of this div element
  * @param idAttribute the name of the id of this div element
  * @return returns a div element with a class and id
  */
-function createDivElement(classAttribute, idAttribute) {
+function createDivElement(onclickAttribute, classAttribute, idAttribute) {
   const divElement = document.createElement('div');
-  divElement.setAttribute("class", classAttribute);
-  divElement.setAttribute("id", idAttribute);
+  divElement.setAttribute('onclick', onclickAttribute);
+  divElement.setAttribute('class', classAttribute);
+  divElement.setAttribute('id', idAttribute);
   return divElement;
+}
+
+/** 
+ * Creates an <i> element containing text, class and id attribute. 
+ *
+ * @param text the text that will be displayed 
+ * @param classAttribute the name of the class of this i element
+ * @param idAttribute the name of the id of this i element
+ * @return returns an i element with text, class, and id
+ */
+function createIElement(text, classAttribute, idAttribute) {
+  const iElement = document.createElement('i');
+  iElement.setAttribute('class', classAttribute);
+  iElement.setAttribute('id', idAttribute);
+  iElement.innerText = text;
+  return iElement;
 }
 
 /** 
@@ -23,8 +43,8 @@ function createDivElement(classAttribute, idAttribute) {
  */
 function createHElement(text, rank, classAttribute, idAttribute) {
   const hElement = document.createElement('h' + rank);
-  hElement.setAttribute("class", classAttribute);
-  hElement.setAttribute("id", idAttribute);  
+  hElement.setAttribute('class', classAttribute);
+  hElement.setAttribute('id', idAttribute);  
   hElement.innerText = text;
   return hElement;
 }
@@ -43,8 +63,8 @@ function createImgElement(srcAttribute, altAttribute, classAttribute,
   const imgElement = document.createElement('img');
   imgElement.src = srcAttribute;
   imgElement.alt = altAttribute;
-  imgElement.setAttribute("class", classAttribute);
-  imgElement.setAttribute("id", idAttribute);
+  imgElement.setAttribute('class', classAttribute);
+  imgElement.setAttribute('id', idAttribute);
   return imgElement;
 }
 
@@ -59,8 +79,8 @@ function createImgElement(srcAttribute, altAttribute, classAttribute,
 function createPElement(text, classAttribute, idAttribute) {
   const pElement = document.createElement('p');
   pElement.innerText = text;
-  pElement.setAttribute("class", classAttribute);
-  pElement.setAttribute("id", idAttribute);
+  pElement.setAttribute('class', classAttribute);
+  pElement.setAttribute('id', idAttribute);
   return pElement;
 }
 
@@ -74,31 +94,72 @@ function createPElement(text, classAttribute, idAttribute) {
  * @param idAttribute the name of the id of this a element 
  * @return returns an a element with the text
  */
-function createAElement(text, targetAttribute, hrefAttribute, classAttribute, idAttribute) {
+function createAElement(text, targetAttribute, hrefAttribute, classAttribute, 
+    idAttribute) {
   const aElement = document.createElement('p');
   aElement.innerText = text;
-  aElement.setAttribute("target", targetAttribute);
-  aElement.setAttribute("href", hrefAttribute);
-  aElement.setAttribute("class", classAttribute);
-  aElement.setAttribute("id", idAttribute);
+  aElement.setAttribute('target', targetAttribute);
+  aElement.setAttribute('href', hrefAttribute);
+  aElement.setAttribute('class', classAttribute);
+  aElement.setAttribute('id', idAttribute);
   return aElement;
 }
+
+// miscellaneous functions
+
+/** 
+ * Toggles the display of an element. 
+ * 
+ * @param id used to get an element with this id
+ */
+function toggleDisplay(display, id) {
+  let element = document.getElementById(id);
+  const elementStyle = getComputedStyle(element, null).display;
+  if (elementStyle === display) {
+    console.log('Element with id ' + id + ' is now hidden.')
+    console.log('Display: ' + elementStyle + ' is now ' + display);
+    element.style.display = 'none';
+  } else {
+    console.log('Element with id ' + id + ' is now visible.')
+    console.log('Display: ' + elementStyle + ' is now ' + display);
+    element.style.display = display;
+  }  
+}
+
+// Listing Page
 
 /**
  * Create an element that shows a listing detailed view
  *
  * @param divCardContainerElement a div element where the listing will pop up
+ * @param number the number assigned to this specific listing (the number 
+ *     itself is insignificant)
  * @return a div with all the information pertaining to a listing
  */
-function createListingDetailedView(divCardContainerElement) {
+function createListingDetailedView(divCardContainerElement, number) {
 
+  const divCardElementId = 'listing' + number;
+  const cardElementDisplay = "flex";
   const divCardElement = createDivElement(
-      "card-information-container shadow-box", "");
+      'toggleDisplay("'+ cardElementDisplay + '", "' + divCardElementId + '")', 
+      'card shadow-box', divCardElementId);
   divCardContainerElement.appendChild(divCardElement);
 
-  divCardElement.appendChild(createListingCardInformation());
+  const divExitElement = createDivElement(
+      '', 'exit', '');
+  divExitElement.appendChild(
+    createIElement('close', 'material-icons', ''));
+  divCardElement.appendChild(divExitElement);
+  
+  const divCardInfoElement = createDivElement(
+      '', 'card-information-container', '');
+  divCardElement.appendChild(divCardInfoElement);
+  
+  console.log('creating listing card information');
+  divCardInfoElement.appendChild(createListingCardInformation());
 
-  divCardElement.appendChild(createListingCardDescription());
+  console.log('creating listing card description');
+  divCardInfoElement.appendChild(createListingCardDescription());
 
   return divCardContainerElement;
 }
@@ -110,23 +171,23 @@ function createListingDetailedView(divCardContainerElement) {
  *     (see below) and website of a listing.
  */
 function createListingCardInformation() {
-  const divCardInformation = createDivElement("card-information", "");
+  const divCardInformation = createDivElement('', 'card-information', '');
   divCardInformation.appendChild(
-      createImgElement("", "picture of listing", "card-picture", ""));
+      createImgElement('', 'picture of listing', 'card-picture', ''));
       
   divCardInformation.appendChild(
-      createHElement("Los Angeles Food Bank", 1, "card-name", ""));
+      createHElement('Los Angeles Food Bank', 1, 'card-name', ''));
 
   divCardInformation.appendChild(
-      createHElement("Category", 2, "category-pill listing-tag", ""));      
+      createHElement('Category', 2, 'category-pill listing-tag', ''));      
 
   divCardInformation.appendChild(
-      createHElement("Reputation: 200 up votes", 2, "reputation-pill", "")); 
+      createHElement('Reputation: 200 up votes', 2, 'reputation-pill', '')); 
 
   divCardInformation.appendChild(createListingDetails()); 
 
   divCardInformation.appendChild(
-      createAElement("Website Link", "_blank", "", "listing-website", "")); 
+      createAElement('Website Link', '_blank', '', 'listing-website', '')); 
 
   return divCardInformation;
 }
@@ -138,19 +199,19 @@ function createListingCardInformation() {
  *     received, its verifitcation, and contact info.
  */
 function createListingDetails() {
-  const divListingDetails = createDivElement("listing-details", "");
+  const divListingDetails = createDivElement('', 'listing-details', '');
   
   divListingDetails.appendChild(
-    createPElement("Listing made on 01/02/20", "", ""));
+    createPElement('Listing made on 01/02/20', '', ''));
 
   divListingDetails.appendChild(
-    createPElement("1,234 Views", "", ""));
+    createPElement('1,234 Views', '', ''));
 
   divListingDetails.appendChild(
-    createPElement("Verified or Community Reputation", "", ""));
+    createPElement('Verified or Community Reputation', '', ''));
 
   divListingDetails.appendChild(
-    createPElement("Contact Info", "", ""));
+    createPElement('Contact Info', '', ''));
 
   return divListingDetails
 }
@@ -161,31 +222,31 @@ function createListingDetails() {
  * @return a div with the description and comments of a listing
  */
 function createListingCardDescription() {
-  const divListingDetails = createDivElement("card-description", "");
+  const divListingDetails = createDivElement('', 'card-description', '');
 
   divListingDetails.appendChild(
-      createHElement("Description", "3", "", "")); 
+      createHElement('Description', '3', '', '')); 
 
   divListingDetails.appendChild(
-    createPElement("Detailed description of what the event/fund/etc is for.", 
-        "", ""));
+    createPElement('Detailed description of what the event/fund/etc is for.', 
+        '', ''));
 
   divListingDetails.appendChild(
-      createHElement("How to help", "3", "", "")); 
+      createHElement('How to help', '3', '', '')); 
 
   divListingDetails.appendChild(
     createPElement(
-        "Detailed description of what the user can do to help the cause. Information about when/where the event is, if physical; links to relevant pages / donation links if digital. \
+        'Detailed description of what the user can do to help the cause. Information about when/where the event is, if physical; links to relevant pages / donation links if digital. \
         Deliver or ship donations to Fake Address 123, Los Angeles, CA 34567 on weekdays. \
-        Volunteer at Fake Address 123, Los Angeles, CA 34567 at 8:30PM everyday", 
-        "", "")); 
+        Volunteer at Fake Address 123, Los Angeles, CA 34567 at 8:30PM everyday', 
+        '', '')); 
   
   divListingDetails.appendChild(
-      createHElement("Comments", "3", "", "")); 
+      createHElement('Comments', '3', '', '')); 
 
   divListingDetails.appendChild(
     createPElement(
-      "Users can write about experience with event/org/etc. Moderation might be touchy though.", "", ""));
+      'Users can write about experience with event/org/etc. Moderation might be touchy though.', '', ''));
 
   return divListingDetails;
 }
