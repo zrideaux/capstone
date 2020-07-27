9b1887f41b64dba66c9bcf0fde080b3382b5d943
Kevin Cam <kevinsebcam@gmail.com>
2020-07-27 20:25:12 +0000

    Update.

--- src/main/webapp/miscellaneous.js
+++ src/main/webapp/miscellaneous.js
@@ -16,3 +16,32 @@ function toggleDisplay(display, id) {
     element.style.display = display;
   }  
 }
+
+/**
+ * Toggle a specified dropdown menu when called. 
+ *
+ * @param menuName text that specifies the id of the menu to be toggled.
+ */
+function toggleDropdown(menuName) {
+  const dropdownMenu = document.getElementById(menuName);
+  let menuIsClosed = (window.getComputedStyle(dropdownMenu).display === 'none');
+
+  // Hide any open dropdown menus.
+  hideDropdownMenus();
+  
+  // Display specified menu if it wasn't already open
+  if (menuIsClosed) {
+    dropdownMenu.style.display = 'block';      
+  }
+}
+
+/**
+ * Hide all dropdown menus when called.
+ */
+function hideDropdownMenus() {
+  const allDropdowns = document.getElementsByClassName('dropdown-menu');
+  for (let i = 0; i < allDropdowns.length; i ++) {
+    allDropdowns[i].style.display = 'none';
+  }
+
+}
