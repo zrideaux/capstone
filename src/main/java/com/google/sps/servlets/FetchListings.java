cb97ee6ea0f4a1cfc41bc4185873c2f80080aaec
Kevin Cam <kevinsebcam@gmail.com>
2020-07-29 22:01:47 +0000

    WIP creating fetchlistings servlet and created utilities folder with Z's getParamter function.

--- src/main/java/com/google/sps/servlets/FetchListings.java
+++ src/main/java/com/google/sps/servlets/FetchListings.java
@@ -15,26 +15,16 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-import java.lang.Math;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
 import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.sps.data.Comment;
-import com.google.sps.utility.CommentConstants;
-import com.google.sps.utility.ValidateInput;
+import com.google.sps.data.Listing;
+import com.google.sps.utility.GetParameter;
 
 /** 
  * Servlet that creates comment objects from entities and returns the list of 
@@ -57,14 +47,12 @@ public class ListCommentsServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) 
       throws IOException {
     // Receive input from the modify number of comments shown form
-    String listingKeys;
-    try {
-      numComments = ValidateInput.getUserNum(request, "num-comments", 1, 
-          CommentConstants.MAX_NUM_COMMENTS);
-    } catch (Exception e) {
-      ValidateInput.createErrorMessage(e, response);
-      return;
-    }  
+    String listingKeys = GetParameter.getParameter(request, "listing-keys", "");
+    
+    if (listingKeys.length() == 0) {
+      // Return error no listings
+      throw new Exception("There are no listing keys.");
+    }
 
     String[] listingKeysArray = listingKeys.split(" ");
     
@@ -74,12 +62,34 @@ public class ListCommentsServlet extends HttpServlet {
     List<Listing> listings = new ArrayList<> ();
 
     for (String listingKey : listingKeysArray) {
-
+      Entity listing = datastore.get(listingKey);
+      listings.add(createListing(listing));
     }
    
 
-    String jsonComments = new Gson().toJson(comments);
+    String jsonListings = new Gson().toJson(listings);
     response.setContentType("application/json;");
-    response.getWriter().println(jsonComments);
+    response.getWriter().println(jsonListings);
+  }
+
+  /**
+   * Creates a Listing object from an Entity object that represents a listing
+   *
+   * @param entity the entity that represents a listing
+   * @return a Listing with all of the properties from the Entity
+   */
+  public static Listing createListing(Entity entity) {
+    String description = entity.getProperty("description");
+    String howToHelp = entity.getProperty("howToHelp");
+    String location = entity.getProperty("location");
+    String name = entity.getProperty("name");
+    long timestamp = entity.getProperty("timestamp");
+    String type = entity.getProperty("type");
+    int upvotes = entity.getProperty("upvotes");
+    int downvotes = entity.getProperty("downvotes");
+    int views = entity.getProperty("views");
+
+    return new Listing(description, howToHelp, location, name, timestamp, type, 
+        upvotes, downvotes, views);
   }
 }
