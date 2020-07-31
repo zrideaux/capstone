64503511754aebac412598331094ff2ed26822f5
zrideaux <zacharyrideaux@gmail.com>
2020-07-31 17:31:08 +0000

    Move files.

--- src/main/webapp/search.js
+++
@@ -1,8 +0,0 @@
-
-import createListing from "/listing.js";
-
-export default function createListings(containerElement) {
-  for (let i = 0; i < 1; i ++) {
-    containerElement.appendChild(createListing(i + 1));
-  }
-}
