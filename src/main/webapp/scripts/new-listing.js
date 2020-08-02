dc47ae61ba0aa40c7dd9331b27378efff1a708d1
Kevin Cam <kevinsebcam@gmail.com>
2020-08-02 21:57:52 +0000

    Get the value of the radio button that has been checked.

--- src/main/webapp/scripts/new-listing.js
+++ src/main/webapp/scripts/new-listing.js
@@ -1,9 +1,8 @@
 import getListings from './listing.js';
 
-import {
-  createPElement
-} from './htmlElement.js';
-
+/**
+ * Creates a listing preview for the user to see
+ */
 export default function displayPreviewListing() {
   const containerElement = document.getElementById("preview");
   let queryString = '/create-listing-preview';
@@ -13,6 +12,11 @@ export default function displayPreviewListing() {
   getListings(containerElement, '', 'preview-listings', queryString);
 }
 
+/**
+ * Creates a string of parameters to query a servlet
+ *
+ * @return creates a string of parameters with their values.
+ */
 function getParameterForQuery() {
   const name = document.getElementById('cause-name').value;
   const location = document.getElementById('cause-location').value;
@@ -21,10 +25,27 @@ function getParameterForQuery() {
   const website = document.getElementById('cause-website').value;
   console.log("Website: " + website);
   let queryParameters = "?name=" + name;
+  queryParameters = queryParameters + "&type=" + getType();
   queryParameters = queryParameters + "&location=" + location;
   queryParameters = queryParameters + "&description=" + description;
   queryParameters = queryParameters + "&howToHelp=" + howToHelp;
   queryParameters = queryParameters + "&website=" + website;
-  queryParameters = queryParameters + "&type=other";
   return queryParameters;
+}
+
+/**
+ * Gets the type of the the listing chosen in the form.
+ *
+ * @return the value of the radio type or an empty sting if none have been 
+ *     chosen.
+ */
+function getType() {
+  const radioTypes = document.getElementsByName("cause-type");
+  for (let i = 0; i < radioTypes.length; i++) {
+    const radioType = radioTypes[i];
+    if (radioType.checked) {
+      return radioType.value;
+    }
+  }
+  return "";
 }
