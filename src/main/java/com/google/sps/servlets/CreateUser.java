dd3528c6984b4a4385b6260646e46136e7223304
zrideaux <zacharyrideaux@gmail.com>
2020-08-03 16:44:46 +0000

    Add downvotedListingKeys property to new user entities.

--- src/main/java/com/google/sps/servlets/CreateUser.java
+++ src/main/java/com/google/sps/servlets/CreateUser.java
@@ -86,6 +86,7 @@ public class CreateUser extends HttpServlet {
     newUserEntity.setProperty("bio", "");
     newUserEntity.setProperty("createdListingKeys", "");
     newUserEntity.setProperty("upvotedListingKeys", "");
+    newUserEntity.setProperty("downvotedListingKeys", "");
 
     datastore.put(newUserEntity);
   }
