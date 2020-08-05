7c23bcfb183f28bdde1cc61ae248374709973768
zrideaux <zacharyrideaux@gmail.com>
2020-08-05 01:07:00 +0000

    Create example front end portion of authentication and rename old file.

--- src/main/webapp/scripts/authentication.js
+++ src/main/webapp/scripts/authentication.js
@@ -1,100 +1,50 @@
 
-// Functions to handle authentication
-
-let auth2;
+// Run authenticate function on page load. 
+window.onload = function() {
+  authenticate();
+};
 
 /**
- * Run on page load. Sets up authentication.
+ * Make a request to Authentication servlet to verify whether user is signed
+ * in. If it is users first time signing in, a user entity will be made.
  */
-function init() {
-  const CLIENT_ID = 'client_id_goes_here';
-  
-  gapi.load('auth2', function() {
-    GoogleAuth = gapi.auth2.init({client_id: CLIENT_ID})
-    .then(() => {
-      renderButton();
-    });
+function authenticate() {
+  fetch('/authentication')
+  .then(response => response.json())
+  .then(authenticationInfo => {
+    console.log(authenticationInfo);
+
+    // Determine if user is logged in or not 
+    let userIsLoggedIn = (authenticationInfo.userIsLoggedIn === 'true');
+
+    const authenticationLink = document.getElementById('authentication-link');
+
+    if (userIsLoggedIn) {
+      // Create logout link.
+      authenticationLink.href = authenticationInfo.logoutLink;
+      authenticationLink.innerText = "Logout";
+    } else {
+      // Create login link.
+      authenticationLink.href = authenticationInfo.loginLink;
+      authenticationLink.innerText = "Login";
+      // Here is an example of changes that can be made if the user is not logged in.
+      hideUserNavIcons();
+      // In the case of user.html and newlisting.html, we can use something
+      // like window.location.replace("/index.html"); to keep non-signed in
+      // users from navigating to them manually in addition to a quick back-end
+      // check when a fetch request is made.
+    }
   });
 }
 
 /**
- * Places the sign in button on the page.
+ * Hide navbar icons that require a user to be logged in to use. 
  */
-function renderButton() {
-  gapi.signin2.render('signInButton', {
-    'scope': 'profile email',
-    'width': 180,
-    'height': 40,
-    'longtitle': false,
-    'theme': 'light',
-    'onsuccess': onSignIn,
-  });
-}
+function hideUserNavIcons() {
+  // Find all page elements with 'user-only' class and set display to none
+  let userOnlyElements = document.getElementsByClassName('user-only');
 
-/**
- * Attaches a google sign in link to a specified element.
- *
- * @param element an html element
- */
-function attachSignin(element) {
-  console.log(element.id);
-  GoogleAuth.attachClickHandler(
-    element,
-    {},
-    function(googleUser) {
-      document.getElementById('name').innerText = "Signed in: " +
-          googleUser.getBasicProfile().getName();
-    },
-    function(error) {
-      alert(JSON.stringify(error, undefined, 2));
-    }
-  );
-}
-
-/**
- * Actions to perform when user first signs in or on page load if
- * user is already signed in.
- */
-function onSignIn(googleUser) {
-  let userProfile = googleUser.getBasicProfile();
-  let idToken = googleUser.getAuthResponse().id_token;
-  console.log('Email: ' + userProfile.getEmail());
-  
-  // Send user's token to CreateUser servlet.
-  const createUserUrl = '/create-user';
-  const http = new XMLHttpRequest();
-
-  http.open('POST', createUserUrl, true);
-  http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 200) {
-      console.log(http.responseText);
-    } else if (http.status >= 400) {
-      console.log(http.responseText);
-      window.alert(http.responseText);
-    }
-  }
-  
-  http.send('idtoken=' + idToken);
-}
-
-/**
- * An example of how to use the front-end portion of the user authentication.
- */
-function example() {
-  // Get the current user and their token.
-  let googleUser = GoogleAuth.currentUser.get();
-  let idToken = googleUser.getAuthResponse().id_token;
-
-  // Use getBasicProfile() to get allowed properties of the account.
-  console.log(googleUser.getBasicProfile().getEmail());
-  
-  // Do a quick front end check to make sure they're signed in.
-  // If they aren't, you might want to do something else like hide
-  //     HTML elements or that kind of thing.
-  if (GoogleAuth.isSignedIn.get()) {
-    // Make sure you also do a check to see they're the right user.
-    fetch('/servlet-name?idtoken=' + idToken);
+  for (let i = 0; i < userOnlyElements.length; i++) {
+    userOnlyElements[i].style.display = 'none';
   }
 }
