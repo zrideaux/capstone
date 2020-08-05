028f2c43df33428a95d8729936dc7b0263321b47
Kevin Cam <kevinsebcam@gmail.com>
2020-08-05 23:29:14 +0000

    Add filter functionality to search page.

--- src/main/java/com/google/sps/servlets/FetchListings.java
+++ src/main/java/com/google/sps/servlets/FetchListings.java
@@ -14,25 +14,28 @@
 
 package com.google.sps.servlets;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.google.gson.Gson;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.gson.Gson;
 import com.google.sps.data.Listing;
 import com.google.sps.utility.ValidateInput;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 /** 
  * Servlet that creates Listings from Entities and returns the list of 
@@ -45,6 +48,8 @@ public class FetchListings extends HttpServlet {
   //     when all filters are checked and separated by "@" (MAX)
   static final int FILTER_MIN = 0;
   static final int FILTER_MAX = 31;
+  // Based on the shortest filter type String length ("other".length = 5)
+  static final int FILTER_MIN_LENGTH = 5;
   // Based on the length of the shortest/longest radius category 
   static final int RADIUS_MIN = 2;
   static final int RADIUS_MAX = 4;
@@ -87,7 +92,7 @@ public class FetchListings extends HttpServlet {
     String sortBy;
     try {
       sortBy = ValidateInput.getUserString(request, 
-        "sort", SORT_MIN, SORT_MAX, "recommended");
+        "sortBy", SORT_MIN, SORT_MAX, "recommended");
     } catch (Exception e) {
       ValidateInput.createErrorMessage(e, response);
       return;
@@ -96,28 +101,13 @@ public class FetchListings extends HttpServlet {
     // Prepare to fetch entities from the backend
     Query queryListing = new Query("Listing");
 
-    // Filter the Listings in the backend
-    int typeFiltersStringLength = typeFiltersString.length();
-    boolean someTypeFiltersChecked = typeFiltersStringLength > 0 && 
-        typeFiltersStringLength < FILTER_MAX;
-    if (someTypeFiltersChecked) {
-      List<Query.Filter> listingTypeFilters = new ArrayList<Query.Filter>();
-
-      String[] typeFilters = typeFiltersString.split("@");
-      for (String typeFilter : typeFilters) {
-        listingTypeFilters.add(new FilterPredicate("type", FilterOperator.EQUAL,
-            typeFilter));
-      }
-      
-      FilterPredicate filterListings = new FilterPredicate("type", 
-          FilterOperator.EQUAL, listingTypeFilter);
-      queryListing = queryListing.setFilter(filterListings);
-    }
+    // Add a type filter for the Listings property if there are any filters
+    filterQuery(typeFiltersString, "type", queryListing);
     
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery preparedQueryListings = datastore.prepare(queryListing);
 
-    FetchOptions entitiesLimit = FetchOptions.Builder.withLimit(10);
+    FetchOptions entitiesLimit = FetchOptions.Builder.withLimit(LISTING_LIMIT);
     List<Entity> listingEntities = preparedQueryListings.asList(entitiesLimit);
 
     // Turn Entities into Listings
@@ -134,5 +124,41 @@ public class FetchListings extends HttpServlet {
     String jsonListings = new Gson().toJson(listings);
     response.setContentType("application/json;");
     response.getWriter().println(jsonListings);
+  }
+
+  /**
+   * Apply a filter to a query if there are some filters that have been checked 
+   *     opposed to no filters checked or all of the filters being checked.
+   * 
+   * @param filtersString The string that contains filters separated by an "@"
+   * @param property The property of the Entity to filter
+   * @param query The query to add a filter to
+   */
+  private void filterQuery(String filtersString, String property, Query query) {
+    int filtersStringLength = filtersString.length();
+    boolean someTypeFiltersChecked = filtersStringLength > FILTER_MIN_LENGTH - 1
+        && filtersStringLength < FILTER_MAX;
+    // If no filters are checked or all of the filters have been checked then 
+    //     don't add any filters
+    if (someTypeFiltersChecked) {
+      String[] typeFilters = filtersString.split("@");
+
+      // If there are more than one filters use a CompositeFilter
+      Filter filter;
+      if (typeFilters.length > 1) {
+        Collection<Filter> filterPredicates = new ArrayList<Filter>();
+        for (String typeFilter : typeFilters) {
+          filterPredicates.add(new FilterPredicate(property, 
+              FilterOperator.EQUAL, typeFilter));
+        }
+
+        filter = new CompositeFilter(CompositeFilterOperator.OR,filterPredicates);
+      // If there are only one filter use a FilterPredicate
+      } else {
+        filter = new FilterPredicate("type", 
+            FilterOperator.EQUAL, typeFilters[0]);
+      }
+      query = query.setFilter(filter);
+    } 
   }
 }
--- src/main/webapp/scripts/search.js
+++ src/main/webapp/scripts/search.js
@@ -1,27 +1,31 @@
-import { 
-  getListings
-} from './listing.js';
+import { getListings } from './listing.js';
 
 import { 
   getCheckboxesByName,
   getRadioByName
 } from './miscellaneous.js';
 
-export default function displayListings(containerElement) {
+/**
+ * Displays listings based on the search parameters (includes type filters, 
+ *     radius filter, and sortBy).
+ */
+export default function displayListings() {
+  const containerElement = document.getElementById("listings");
+  containerElement.innerHTML = '';
   const queryString = '/fetch-listings?' + getSearchParameters();
   getListings(containerElement, '', 'search-listings', queryString);
 }
 
 /**
+ * Get the search parameters (type filters, radius filter, and sortBy) from the 
+ *     search page.
  */
 function getSearchParameters() {
   const filterTypes = getCheckboxesByName('search-type-option');
-  console.log("FILTER type: " + filterTypes);
-  console.log("LENGTH filter type: " + filterTypes.length);
   const filterRadius = getRadioByName('search-radius-option');
   const sort = getRadioByName('search-sort-option');
   let param = 'type-filters=' + filterTypes;
-  param += '&radius-filter' + filterRadius;
-  param += '&sort' + sort;
+  param += '&radius-filter=' + filterRadius;
+  param += '&sortBy=' + sort;
   return param;
 }
