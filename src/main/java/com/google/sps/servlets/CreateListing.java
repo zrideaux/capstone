775c2ad87baf3bed5d3d887073d567e5b3bb552a
zrideaux <zacharyrideaux@gmail.com>
2020-08-05 18:54:58 +0000

    Add check to ensure user is logged in to CreateListing.java.

--- src/main/java/com/google/sps/servlets/CreateListing.java
+++ src/main/java/com/google/sps/servlets/CreateListing.java
@@ -37,85 +37,92 @@ public class CreateListing extends HttpServlet {
   /** Uses getParmeter function to obtain user input and inserts that input into  Entity for storage.*/
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        // The following variables are required and have a max char limit
-    String description;
-    try {
-      description = ValidateInput.getUserString(request, "description", 1, 
-          ListingConstants.MAX_CONTENT_LEN);
-    } catch (Exception e) {
-      ValidateInput.createErrorMessage(e, response);
-      return;
-    }  
 
-    String howToHelp;
-    try {
-      howToHelp = ValidateInput.getUserString(request, "howToHelp", 1, 
-          ListingConstants.MAX_CONTENT_LEN);
-    } catch (Exception e) {
-      ValidateInput.createErrorMessage(e, response);
-      return;
-    }  
-
-    String location;
-    try {
-      location = ValidateInput.getUserString(request, "location", 1, 
-          ListingConstants.MAX_LOCATION_LEN);
-    } catch (Exception e) {
-      ValidateInput.createErrorMessage(e, response);
-      return;
-    } 
-
-    String name;
-    try {
-      name = ValidateInput.getUserString(request, "name", 1, 
-          ListingConstants.MAX_NAME_LEN);
-    } catch (Exception e) {
-      ValidateInput.createErrorMessage(e, response);
-      return;
-    } 
-
-    String type;
-    try {
-      type = ValidateInput.getUserString(request, "type", 1, 
-          ListingConstants.MAX_TYPE_LEN);
-    } catch (Exception e) {
-      ValidateInput.createErrorMessage(e, response);
-      return;
-    } 
-
-    // Uploading an image is optional
-    String imageURL = ValidateInput.getUploadedFileUrl(request, "image", ""); 
-
-    // There are no char limit for website and website is optional
-    String website = ValidateInput.getParameter(request, "website", "");
-  
-    long timestamp = System.currentTimeMillis();
-
-    Entity listingEntity = new Entity("Listing");
-    listingEntity.setProperty("description", description);
-    listingEntity.setProperty("howToHelp", howToHelp);
-    listingEntity.setProperty("imageURL", imageURL);
-    listingEntity.setProperty("location", location);
-    listingEntity.setProperty("name", name);
-    listingEntity.setProperty("timestamp",timestamp);
-    listingEntity.setProperty("type", type);
-    listingEntity.setProperty("upvotes", 0);
-    listingEntity.setProperty("downvotes", 0);
-    listingEntity.setProperty("views", 0);
-    listingEntity.setProperty("website", website);
-
-    // Place the new listing entity in datastore and save its key
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();    
-    Key listingEntityKey = datastore.put(listingEntity);
-    
-    // Get current user and place the entity in their created listings property
     UserService userService = UserServiceFactory.getUserService();
-    Entity currentUser = AuthenticationUtility.getCurrentUserEntity(
-        datastore, userService);
-    User.addListingKeyToUserEntity(datastore, currentUser, 
-        listingEntityKey, "createdListingKeys");
 
-    // Returns a success message since everything went smoothly
-    ValidateInput.createSuccessMessage(response);
+    // Only allow user to make listing if logged in
+    if (userService.isUserLoggedIn()) {
+      // The following variables are required and have a max char limit
+      String description;
+      try {
+        description = ValidateInput.getUserString(request, "description", 1,
+            ListingConstants.MAX_CONTENT_LEN);
+      } catch (Exception e) {
+        ValidateInput.createErrorMessage(e, response);
+        return;
+      }
+
+      String howToHelp;
+      try {
+        howToHelp = ValidateInput.getUserString(request, "howToHelp", 1,
+            ListingConstants.MAX_CONTENT_LEN);
+      } catch (Exception e) {
+        ValidateInput.createErrorMessage(e, response);
+        return;
+      } 
+
+      String location;
+      try {
+        location = ValidateInput.getUserString(request, "location", 1,
+            ListingConstants.MAX_LOCATION_LEN);
+      } catch (Exception e) {
+        ValidateInput.createErrorMessage(e, response);
+        return;
+      }
+
+      String name;
+      try {
+        name = ValidateInput.getUserString(request, "name", 1,
+            ListingConstants.MAX_NAME_LEN);
+      } catch (Exception e) {
+        ValidateInput.createErrorMessage(e, response);
+        return;
+      }
+
+      String type;
+      try {
+        type = ValidateInput.getUserString(request, "type", 1,
+            ListingConstants.MAX_TYPE_LEN);
+      } catch (Exception e) {
+        ValidateInput.createErrorMessage(e, response);
+        return;
+      }
+
+      // Uploading an image is optional
+      String imageURL = ValidateInput.getUploadedFileUrl(request, "image", "");
+
+      // There are no char limit for website and website is optional
+      String website = ValidateInput.getParameter(request, "website", "");
+    
+      long timestamp = System.currentTimeMillis();
+
+      Entity listingEntity = new Entity("Listing");
+      listingEntity.setProperty("description", description);
+      listingEntity.setProperty("howToHelp", howToHelp);
+      listingEntity.setProperty("imageURL", imageURL);
+      listingEntity.setProperty("location", location);
+      listingEntity.setProperty("name", name);
+      listingEntity.setProperty("timestamp",timestamp);
+      listingEntity.setProperty("type", type);
+      listingEntity.setProperty("upvotes", 0);
+      listingEntity.setProperty("downvotes", 0);
+      listingEntity.setProperty("views", 0);
+      listingEntity.setProperty("website", website);
+
+      // Place the new listing entity in datastore and save its key
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      Key listingEntityKey = datastore.put(listingEntity);
+
+      // Get current user and place the entity in their created listings property
+      Entity currentUser = AuthenticationUtility.getCurrentUserEntity(
+          datastore, userService);
+      User.addListingKeyToUserEntity(datastore, currentUser,
+          listingEntityKey, "createdListingKeys");
+
+      // Returns a success message since everything went smoothly
+      ValidateInput.createSuccessMessage(response);
+    } else {
+      ValidateInput.createErrorMessage("User is not logged in.", response);
+    }
   }
 }
