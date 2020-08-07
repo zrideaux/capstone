95b3093addaf07865de594eae4854a7157ded638
Kevin Cam <kevinsebcam@gmail.com>
2020-08-07 21:16:10 +0000

    Create a function that makes an element keyboard accessible through the tab and enter key.

--- src/main/webapp/scripts/search.js
+++ src/main/webapp/scripts/search.js
@@ -3,13 +3,18 @@ import { authenticate } from './authentication.js';
 import { getListings } from './listing.js';
 
 import { 	
+  checkAllCheckboxes,
   getCheckboxesByName,	
-  getRadioByName	
+  getRadioByName,
+  keyboardAccessible,
+  keyboardAccessibleOnClick,
+  mapElementsByName
 } from './miscellaneous.js';	
 
 
 window.onload = function() {
-  authenticate();
+
+  authenticate(addOnclickToInputs);
 }
 
 /**
@@ -24,6 +29,61 @@ export default function displayListings() {
 }
 
 /**
+ * 
+ */
+function addOnclickToInputs() {
+  const seeAllElement = document.getElementById('see-all');
+  keyboardAccessible(seeAllElement, displayListingsShowAllFilters, "0");
+ 
+  // Checkbox is tab accessible
+  // const displayListingsOnClickCheckbox = (checkbox) => {
+  //   const changeCheckedAndDisplayListings = () => {
+  //     checkbox.checked = !checkbox.checked;
+  //     displayListings();
+  //   }
+  //   keyboardAccessibleOnClick(checkbox, displayListings, 
+  //       changeCheckedAndDisplayListings);
+  // };
+  mapElementsByName(displayListingsOnClickCheckbox, 'search-type-option');
+
+  // Radio is not tab accessible
+  const displayListingsOnClickRadio = (radio) => {
+    keyboardAccessibleOnClick(radio, displayListings, displayListings);
+  }
+
+  mapElementsByName(displayListingsOnClickRadio, 'search-radius-option');
+  mapElementsByName(displayListingsOnClickRadio, 'search-sort-option');
+}
+
+/**
+ * Add onclick and when enter key is pressed event listeners to the checkbox 
+ *     element.
+ * This function is used to be passed as a parameter to the mapElementsByName 
+ *     func, and can only have one parameter.
+ *
+ * @param checkbox the checkbox element that will be given event listeners
+ */
+function displayListingsOnClickCheckbox(checkbox) {
+  // On enter key, uncheck/check the checkbox and fetch listings
+  const changeCheckedAndDisplayListings = () => {
+    checkbox.checked = !checkbox.checked;
+    displayListings();
+  }
+
+  keyboardAccessibleOnClick(checkbox, displayListings, 
+      changeCheckedAndDisplayListings);
+};
+
+/**
+ * Display listings based on the search parameters and make all filter 
+ *     checkboxes checked.
+ */
+function displayListingsShowAllFilters() {
+  checkAllCheckboxes('search-type-option');
+  displayListings();
+}
+
+/**
  * Get the search parameters (type filters, radius filter, and sortBy) from the 
  *     search page.
  *
@@ -33,8 +93,10 @@ function getSearchParameters() {
   const filterTypes = getCheckboxesByName('search-type-option');
   const filterRadius = getRadioByName('search-radius-option');
   const sort = getRadioByName('search-sort-option');
+  const location = document.getElementById('search-location-input').value;
   let param = 'type-filters=' + filterTypes;
   param += '&radius-filter=' + filterRadius;
   param += '&sortBy=' + sort;
+  param += '&location=' + location;
   return param;
 }
