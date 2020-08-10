d40ba31847f4288e829bc8927742de5d1dca1587
Kevin Cam <kevinsebcam@gmail.com>
2020-08-10 17:06:48 +0000

    Remove println statements.

--- src/main/java/com/google/sps/filter/FilterQuery.java
+++ src/main/java/com/google/sps/filter/FilterQuery.java
@@ -26,56 +26,38 @@ import java.util.Collection;
 import java.util.HashMap;
 
 public final class FilterQuery {
-
-  static final HashMap<String, String> FILTERS = new HashMap<String, String>();
-
-  /**
-   * Populate the FILTERS HashMap constant
-   */
-  private static void initializeFilters() {
-    FILTERS.put("1", "fundraiser");
-    FILTERS.put("2", "petition");
-    FILTERS.put("3", "event");
-    FILTERS.put("4", "other");
-  }
-
   /**
    * Apply a filter to a query if some filters have been checked as opposed to 
    *     no filters checked or all filters checked.
    * 
-   * @param filtersString The string that contains filters separated by an "@"
+   * @param filtersKeyString The string that contains filter keys separated by 
+   *     an "@".
+   * @param filters A Hashmap used to get the filter value from the filter keys.
    * @param property The property of the Entity to filter
    * @param query The query to add a filter to
    */
-  public static void filterQuery(String filtersString, String property, 
-      Query query) {
-    int filtersStringLength = filtersString.length();
-
+  public static void filterQuery(String filtersKeyString, 
+      HashMap<String, String> filters, String property, Query query) {
     // If no filters are checked (String length = FILTER_MIN) or all of the 
     //     filters have been checked (FILTER_MAX) then don't add any filters.
-    boolean someTypeFiltersChecked = filtersStringLength > 
-        ListingConstants.FILTER_MIN && filtersStringLength < 
-        ListingConstants.FILTER_MAX;
+    String[] typeFilters = filtersKeyString.split("@");
+    boolean someTypeFiltersChecked = filtersKeyString.length() > 0 && 
+        typeFilters.length < filters.size();
     if (someTypeFiltersChecked) {
-      // Adds values to the FILTERS HasMap constant if there are filters
-      initializeFilters();
-
-      String[] typeFilters = filtersString.split("@");
-
       // If there are more than one filters use a CompositeFilter
       Filter filter;
       if (typeFilters.length > 1) {
         Collection<Filter> filterPredicates = new ArrayList<Filter>();
         for (String typeFilter : typeFilters) {
           filterPredicates.add(new FilterPredicate(property, 
-              FilterOperator.EQUAL, FILTERS.get(typeFilter)));
+              FilterOperator.EQUAL, filters.get(typeFilter)));
         }
 
         filter = new CompositeFilter(CompositeFilterOperator.OR,filterPredicates);
       // If there are only one filter use a FilterPredicate
       } else {
-        filter = new FilterPredicate("type", 
-            FilterOperator.EQUAL, FILTERS.get(typeFilters[0]));
+        filter = new FilterPredicate(property, 
+            FilterOperator.EQUAL, filters.get(typeFilters[0]));
       }
       query = query.setFilter(filter);
     } 
