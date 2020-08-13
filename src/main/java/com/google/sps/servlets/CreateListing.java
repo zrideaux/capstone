d255a2ab8f85a7485cb2c7114b903e526b34301a
zrideaux <zacharyrideaux@gmail.com>
2020-08-13 01:40:43 +0000

    Add default image to new listings and listing previews.

--- src/main/java/com/google/sps/servlets/CreateListing.java
+++ src/main/java/com/google/sps/servlets/CreateListing.java
@@ -89,7 +89,7 @@ public class CreateListing extends HttpServlet {
       }
 
       // Uploading an image is optional
-      String imageURL = ValidateInput.getUploadedFileUrl(request, "image", "");
+      String imageURL = ValidateInput.getUploadedFileUrl(request, "image", "https://i.imgur.com/wE5wDVZ.png");
 
       // There are no char limit for website and website is optional
       String website = ValidateInput.getParameter(request, "website", "");
