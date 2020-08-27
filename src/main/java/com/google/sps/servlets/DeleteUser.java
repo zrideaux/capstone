78d38ce874d47459e2151c2ad0dc9d84a44b64a2
Kevin Cam <kevinsebcam@gmail.com>
2020-08-27 21:15:28 +0000

    Create delete user functionality.

--- src/main/java/com/google/sps/servlets/DeleteUser.java
+++ src/main/java/com/google/sps/servlets/DeleteUser.java
@@ -1,46 +1,66 @@
-// // Copyright 2019 Google LLC
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     https://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
  
-// package com.google.sps.servlets;
+package com.google.sps.servlets;
 
-// import java.io.IOException;
-// import javax.servlet.annotation.WebServlet;
-// import javax.servlet.http.HttpServlet;
-// import javax.servlet.http.HttpServletResponse;
-// import javax.servlet.http.HttpServletRequest;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.sps.data.Listing;
+import com.google.sps.data.User;
+import com.google.sps.utility.AuthenticationUtility;
+import com.google.sps.utility.DeleteUserUtility;
+import com.google.sps.utility.ValidateInput;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
-// @WebServlet
-// public class DeleteUser extends HttpServlet {
+@WebServlet("/delete-user")
+public class DeleteUser extends HttpServlet {
 
-//   @Override
-//   public void doPost(HttpServletRequest request, HttpServletResponse response)
-//       throws IOException {
-//     UserService userService = UserServiceFactory.getUserService();
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
 
-//     if (userService.isUserLoggedIn()) {
-//       String userEmail = userService.getCurrentUser().getEmail();
-//       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    if (userService.isUserLoggedIn()) {
+      String userEmail = userService.getCurrentUser().getEmail();
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-//       List<Entity> ownedListingEntities = User.getCreatedListings(datastore,
-//           userEmail);
-      
+      try {
+        DeleteUserUtility.deleteUsersCreatedListings(datastore, userEmail);
+      } catch (Exception e) {
+        ValidateInput.createErrorMessage(e, response);
+        return;
+      }
 
-//     }
-//     // Delete all of its listings
-    
+      // Undo any likes/dislikes
+      // Get user entity and get their upvoted/downvoted listing key strings
+      Entity userEntity = AuthenticationUtility.getUserByEmail(datastore, 
+          userEmail);
 
-//     // Undo any likes/dislikes
+      datastore.delete(userEntity.getKey());
 
-//   }
-// }
+      ValidateInput.createSuccessMessage(response);
+    } else {
+      ValidateInput.createErrorMessage("User needs to log in", response);
+    }
+  }
+}
---
+++ src/main/java/com/google/sps/utility/DeleteUserUtility.java
@@ -0,0 +1,41 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.utility;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.sps.data.User;
+import com.google.sps.utility.DeleteListingUtility;
+import java.util.List;
+
+public final class DeleteUserUtility {
+  /**
+   * Delete all of a user's created listings.
+   *
+   * @param datastore the DatastoreService that connects to 
+   *     the database.
+   * @param userEmail the email of the user whose created listings will be 
+   *     deleted.
+   */
+  public static void deleteUsersCreatedListings(DatastoreService datastore,
+      String userEmail) throws Exception {
+    List<Entity> ownedListingEntities = User.getCreatedListings(datastore,
+          userEmail);
+
+    for (Entity listingEntity : ownedListingEntities) {
+      DeleteListingUtility.deleteListing(datastore, listingEntity);
+    }
+  }
+}
